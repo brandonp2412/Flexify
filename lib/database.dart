7814615a68ebb5565f7bc7c4412b4c71ae98f022
Brandon Presley <brandon@presley.nz>
2024-02-23 12:42:35 +1300

    Log SQL statements in debug mode

--- lib/database.dart
+++ lib/database.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:drift/drift.dart';
 import 'package:drift/native.dart';
+import 'package:flutter/foundation.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:path/path.dart' as p;
 import 'package:sqlite3/sqlite3.dart';
@@ -42,6 +43,7 @@ LazyDatabase _openConnection() {
 
     final cachebase = (await getTemporaryDirectory()).path;
     sqlite3.tempDirectory = cachebase;
-    return NativeDatabase.createInBackground(file);
+    return NativeDatabase.createInBackground(file,
+        logStatements: kDebugMode ? true : false);
   });
 }
