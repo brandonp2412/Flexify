2f762f181b2a394e720f7bcde8e497626fcbfad1
Brandon Presley <brandon@presley.nz>
2024-02-23 13:22:59 +1300

    Move progress query to start plan page
    Now we don't do N queries for each
    exercise.

--- lib/exercise_tile.dart
+++ lib/exercise_tile.dart
@@ -1,68 +1,36 @@
-import 'package:drift/drift.dart';
-import 'package:flexify/main.dart';
 import 'package:flutter/material.dart';
 
-class ExerciseTile extends StatefulWidget {
+class ExerciseTile extends StatelessWidget {
   final String exercise;
   final bool isSelected;
   final VoidCallback onTap;
+  final double progress;
 
   const ExerciseTile({
     Key? key,
     required this.exercise,
     required this.isSelected,
     required this.onTap,
+    required this.progress,
   }) : super(key: key);
-
-  @override
-  State<ExerciseTile> createState() => _ExerciseTileState();
-}
-
-class _ExerciseTileState extends State<ExerciseTile> {
-  late Stream<TypedResult> stream;
-
-  @override
-  void initState() {
-    super.initState();
-    var countExp = database.gymSets.name.count();
-    final today = DateTime.now();
-    final startOfToday = DateTime(today.year, today.month, today.day);
-    final startOfTomorrow = startOfToday.add(const Duration(days: 1));
-
-    stream = (database.selectOnly(database.gymSets)
-          ..addColumns([countExp, database.gymSets.name])
-          ..where(database.gymSets.created.isBiggerOrEqualValue(startOfToday))
-          ..where(database.gymSets.created.isSmallerThanValue(startOfTomorrow))
-          ..where(database.gymSets.name.equals(widget.exercise)))
-        .watchSingle();
-  }
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      onTap: widget.onTap,
+      onTap: onTap,
       title: Row(children: [
         Radio(
-          value: widget.isSelected,
+          value: isSelected,
           groupValue: true,
           onChanged: (value) {
-            widget.onTap();
+            onTap();
           },
         ),
-        Text(widget.exercise),
+        Text(exercise),
       ]),
       subtitle: SizedBox(
-        child: StreamBuilder<TypedResult>(
-          stream: stream,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData)
-              return const LinearProgressIndicator(value: 0);
-            var countExp = database.gymSets.name.count();
-            final count = snapshot.data!.read(countExp) ?? 0;
-            return LinearProgressIndicator(
-              value: count / 5,
-            );
-          },
+        child: LinearProgressIndicator(
+          value: progress,
         ),
       ),
     );
--- lib/start_plan_page.dart
+++ lib/start_plan_page.dart
@@ -1,3 +1,4 @@
+import 'package:drift/drift.dart';
 import 'package:flexify/database.dart';
 import 'package:flexify/main.dart';
 import 'package:flutter/material.dart';
@@ -10,29 +11,38 @@ class StartPlanPage extends StatefulWidget {
   const StartPlanPage({Key? key, required this.plan}) : super(key: key);
 
   @override
-  _StartPlanPageState createState() => _StartPlanPageState();
+  createState() => _StartPlanPageState();
 }
 
 class _StartPlanPageState extends State<StartPlanPage> {
   late TextEditingController _repsController;
   late TextEditingController _weightController;
-  late TextEditingController _unitController;
+  late Stream<List<TypedResult>> stream;
 
   int _selectedExerciseIndex = 0;
+  String _selectedUnit = 'kg';
 
   @override
   void initState() {
     super.initState();
-    _repsController = TextEditingController();
-    _weightController = TextEditingController();
-    _unitController = TextEditingController();
+    _repsController = TextEditingController(text: "10");
+    _weightController = TextEditingController(text: "20");
+
+    final today = DateTime.now();
+    final startOfToday = DateTime(today.year, today.month, today.day);
+    final startOfTomorrow = startOfToday.add(const Duration(days: 1));
+    stream = (database.selectOnly(database.gymSets)
+          ..addColumns([database.gymSets.name.count(), database.gymSets.name])
+          ..where(database.gymSets.created.isBiggerOrEqualValue(startOfToday))
+          ..where(database.gymSets.created.isSmallerThanValue(startOfTomorrow))
+          ..groupBy([database.gymSets.name]))
+        .watch();
   }
 
   @override
   void dispose() {
     _repsController.dispose();
     _weightController.dispose();
-    _unitController.dispose();
     super.dispose();
   }
 
@@ -58,23 +68,47 @@ class _StartPlanPageState extends State<StartPlanPage> {
             decoration: const InputDecoration(labelText: 'Weight'),
             keyboardType: TextInputType.number,
           ),
-          TextField(
-            controller: _unitController,
-            decoration: const InputDecoration(labelText: 'Unit'),
+          DropdownButtonFormField<String>(
+            value: _selectedUnit,
+            onChanged: (newValue) {
+              setState(() {
+                _selectedUnit = newValue!;
+              });
+            },
+            items: <String>['kg', 'lb']
+                .map<DropdownMenuItem<String>>((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value),
+              );
+            }).toList(),
           ),
           const SizedBox(height: 20),
-          ListView.builder(
-            shrinkWrap: true,
-            itemCount: exercises.length,
-            itemBuilder: (context, index) {
-              final exercise = exercises[index];
-              return ExerciseTile(
-                exercise: exercise,
-                isSelected: index == _selectedExerciseIndex,
-                onTap: () {
-                  setState(() {
-                    _selectedExerciseIndex = index;
-                  });
+          StreamBuilder(
+            stream: stream,
+            builder: (context, snapshot) {
+              if (!snapshot.hasData) return const SizedBox();
+
+              return ListView.builder(
+                shrinkWrap: true,
+                itemCount: exercises.length,
+                itemBuilder: (context, index) {
+                  final exercise = exercises[index];
+                  final gymSet = snapshot.data?.where((element) =>
+                      element.read(database.gymSets.name) == exercise);
+                  var count = 0;
+                  if (gymSet != null && gymSet.isNotEmpty)
+                    count = gymSet.first.read(database.gymSets.name.count())!;
+                  return ExerciseTile(
+                    exercise: exercise,
+                    isSelected: index == _selectedExerciseIndex,
+                    progress: count / 5,
+                    onTap: () {
+                      setState(() {
+                        _selectedExerciseIndex = index;
+                      });
+                    },
+                  );
                 },
               );
             },
@@ -86,7 +120,7 @@ class _StartPlanPageState extends State<StartPlanPage> {
         onPressed: () {
           final reps = int.parse(_repsController.text);
           final weight = int.parse(_weightController.text);
-          final unit = _unitController.text;
+          final unit = _selectedUnit;
           final exercise = exercises[_selectedExerciseIndex];
 
           final gymSet = GymSetsCompanion.insert(
