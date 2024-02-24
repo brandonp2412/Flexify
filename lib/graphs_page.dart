f5ef2cf0aeff52d98a5c3eeb8193f2d18417bf51
Brandon Presley <brandon@presley.nz>
2024-02-24 19:33:59 +1300

    Add export to CSV button to graphs

--- lib/graphs_page.dart
+++ lib/graphs_page.dart
@@ -78,78 +78,9 @@ class _GraphsPageState extends State<GraphsPage> {
                       PopupMenuButton(
                         icon: const Icon(Icons.more_vert),
                         itemBuilder: (context) => [
-                          PopupMenuItem(
-                            child: ListTile(
-                              leading: const Icon(Icons.upload),
-                              title: const Text('Upload csv'),
-                              onTap: () async {
-                                Navigator.pop(context);
-                                final result =
-                                    await FilePicker.platform.pickFiles(
-                                  type: FileType.any,
-                                );
-                                if (result == null) return;
-
-                                final file = File(result.files.single.path!);
-                                final input = file.openRead();
-                                final fields = await input
-                                    .transform(utf8.decoder)
-                                    .transform(
-                                        const CsvToListConverter(eol: "\n"))
-                                    .skip(1)
-                                    .toList();
-                                final gymSets = fields.map(
-                                  (row) => GymSetsCompanion(
-                                    name: drift.Value(row[1]),
-                                    reps: drift.Value(double.parse(row[2])),
-                                    weight: drift.Value(double.parse(row[3])),
-                                    created: drift.Value(parseDate(row[4])),
-                                    unit: drift.Value(row[5]),
-                                  ),
-                                );
-                                await database.batch(
-                                  (batch) => batch.insertAll(
-                                      database.gymSets, gymSets),
-                                );
-                              },
-                            ),
-                          ),
-                          PopupMenuItem(
-                            child: ListTile(
-                              leading: const Icon(Icons.delete),
-                              title: const Text('Delete all records'),
-                              onTap: () {
-                                Navigator.of(context).pop();
-                                showDialog(
-                                  context: context,
-                                  builder: (BuildContext context) {
-                                    return AlertDialog(
-                                      title: const Text('Confirm Delete'),
-                                      content: const Text(
-                                          'Are you sure you want to delete all records? This action is not reversible.'),
-                                      actions: <Widget>[
-                                        TextButton(
-                                          child: const Text('Cancel'),
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                        ),
-                                        TextButton(
-                                          child: const Text('Delete'),
-                                          onPressed: () async {
-                                            Navigator.of(context).pop();
-                                            await database
-                                                .delete(database.gymSets)
-                                                .go();
-                                          },
-                                        ),
-                                      ],
-                                    );
-                                  },
-                                );
-                              },
-                            ),
-                          ),
+                          exportCsv(context),
+                          uploadCsv(context),
+                          deleteAll(context),
                         ],
                       )
                     ],
@@ -186,6 +117,117 @@ class _GraphsPageState extends State<GraphsPage> {
             },
           )
         ],
+      ),
+    );
+  }
+
+  PopupMenuItem<dynamic> deleteAll(BuildContext context) {
+    return PopupMenuItem(
+      child: ListTile(
+        leading: const Icon(Icons.delete),
+        title: const Text('Delete all records'),
+        onTap: () {
+          Navigator.of(context).pop();
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: const Text('Confirm Delete'),
+                content: const Text(
+                    'Are you sure you want to delete all records? This action is not reversible.'),
+                actions: <Widget>[
+                  TextButton(
+                    child: const Text('Cancel'),
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  TextButton(
+                    child: const Text('Delete'),
+                    onPressed: () async {
+                      Navigator.of(context).pop();
+                      await database.delete(database.gymSets).go();
+                    },
+                  ),
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+
+  PopupMenuItem<dynamic> uploadCsv(BuildContext context) {
+    return PopupMenuItem(
+      child: ListTile(
+        leading: const Icon(Icons.upload),
+        title: const Text('Upload CSV'),
+        onTap: () async {
+          Navigator.pop(context);
+          final result = await FilePicker.platform.pickFiles(
+            type: FileType.any,
+          );
+          if (result == null) return;
+
+          final file = File(result.files.single.path!);
+          final input = file.openRead();
+          final fields = await input
+              .transform(utf8.decoder)
+              .transform(const CsvToListConverter(eol: "\n"))
+              .skip(1)
+              .toList();
+          final gymSets = fields.map(
+            (row) => GymSetsCompanion(
+              name: drift.Value(row[1]),
+              reps: drift.Value(double.parse(row[2])),
+              weight: drift.Value(double.parse(row[3])),
+              created: drift.Value(parseDate(row[4])),
+              unit: drift.Value(row[5]),
+            ),
+          );
+          await database.batch(
+            (batch) => batch.insertAll(database.gymSets, gymSets),
+          );
+        },
+      ),
+    );
+  }
+
+  PopupMenuItem<dynamic> exportCsv(BuildContext context) {
+    return PopupMenuItem(
+      child: ListTile(
+        leading: const Icon(Icons.download),
+        title: const Text('Export to CSV'),
+        onTap: () async {
+          Navigator.pop(context);
+          final result = await FilePicker.platform.getDirectoryPath();
+          if (result == null) return;
+
+          final gymSets = await database.gymSets.select().get();
+          final List<List<dynamic>> csvData = [
+            ['id', 'name', 'reps', 'weight', 'unit', 'created']
+          ];
+          for (var gymSet in gymSets) {
+            csvData.add([
+              gymSet.id,
+              gymSet.name,
+              gymSet.reps,
+              gymSet.weight,
+              gymSet.unit,
+              gymSet.created.toIso8601String(),
+            ]);
+          }
+
+          final file = File("$result/gym_sets.csv");
+          String csv = const ListToCsvConverter().convert(csvData);
+          await file.writeAsString(csv);
+          if (!mounted) return;
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(
+                content: Text('Exported to CSV.'), showCloseIcon: true),
+          );
+        },
       ),
     );
   }
