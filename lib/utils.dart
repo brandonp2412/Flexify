624118284901d8a52090f5e69c9497ea660b3257
Brandon Presley <brandon@presley.nz>
2024-02-25 18:37:26 +1300

    Add another date format to parseDate

--- lib/utils.dart
+++ lib/utils.dart
@@ -2,9 +2,10 @@ import 'package:intl/intl.dart';
 
 DateTime parseDate(String dateString) {
   List<String> formats = [
+    'dd.MM.yyyy',
     'yyyy-MM-ddTHH:mm',
     'yyyy-MM-ddTHH:mm:ss.SSS',
-    'yyyy-MM-ddTHH:mm:ss'
+    'yyyy-MM-ddTHH:mm:ss',
   ];
 
   for (String format in formats) {
