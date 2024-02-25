d23fe8500d3bd36a12780aa1c97e220586886606
Brandon Presley <brandon@presley.nz>
2024-02-25 19:24:02 +1300

    Make delete a dropdown in exercise tile

--- lib/exercise_tile.dart
+++ lib/exercise_tile.dart
@@ -18,33 +18,59 @@ class ExerciseTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      onTap: onTap,
-      onLongPress: () async {
+    return GestureDetector(
+      onLongPressStart: (details) async {
         if (count == 0) return;
-        final gymSet = await (database.select(database.gymSets)
-              ..where((r) => database.gymSets.name.equals(exercise))
-              ..orderBy([
-                (u) => OrderingTerm(
-                    expression: u.created, mode: OrderingMode.desc),
-              ])
-              ..limit(1))
-            .getSingle();
-        await database.gymSets.deleteOne(gymSet);
+
+        final position = RelativeRect.fromLTRB(
+          details.globalPosition.dx,
+          details.globalPosition.dy,
+          MediaQuery.of(context).size.width - details.globalPosition.dx,
+          MediaQuery.of(context).size.height - details.globalPosition.dy,
+        );
+
+        await showMenu(
+          context: context,
+          position: position,
+          items: [
+            PopupMenuItem(
+              value: 'delete',
+              child: ListTile(
+                leading: const Icon(Icons.delete),
+                title: const Text("Delete"),
+                onTap: () async {
+                  Navigator.pop(context);
+                  final gymSet = await (database.select(database.gymSets)
+                        ..where((r) => database.gymSets.name.equals(exercise))
+                        ..orderBy([
+                          (u) => OrderingTerm(
+                              expression: u.created, mode: OrderingMode.desc),
+                        ])
+                        ..limit(1))
+                      .getSingle();
+                  await database.gymSets.deleteOne(gymSet);
+                },
+              ),
+            ),
+          ],
+        );
       },
-      title: Row(children: [
-        Radio(
-          value: isSelected,
-          groupValue: true,
-          onChanged: (value) {
-            onTap();
-          },
-        ),
-        Text("$exercise ($count)"),
-      ]),
-      subtitle: SizedBox(
-        child: LinearProgressIndicator(
-          value: count / 5,
+      child: ListTile(
+        onTap: onTap,
+        title: Row(children: [
+          Radio(
+            value: isSelected,
+            groupValue: true,
+            onChanged: (value) {
+              onTap();
+            },
+          ),
+          Text("$exercise ($count)"),
+        ]),
+        subtitle: SizedBox(
+          child: LinearProgressIndicator(
+            value: count / 5,
+          ),
         ),
       ),
     );
