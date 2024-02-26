022e60c330d8b8b29854e76dc1bc75f6287fee37
Brandon Presley <brandon@presley.nz>
2024-02-27 11:03:57 +1300

    Fix references to old package name

--- android/app/src/main/kotlin/com/example/flexify/StopAlarm.kt
+++ android/app/src/main/kotlin/com/example/flexify/StopAlarm.kt
@@ -1,4 +1,4 @@
-package com.example.flexify
+package com.flexify
 
 import android.app.Activity
 import android.content.Intent
--- android/app/src/main/kotlin/com/example/flexify/TimerService.kt
+++ android/app/src/main/kotlin/com/example/flexify/TimerService.kt
@@ -1,4 +1,4 @@
-package com.example.flexify
+package com.flexify
 
 import android.Manifest
 import android.annotation.SuppressLint
