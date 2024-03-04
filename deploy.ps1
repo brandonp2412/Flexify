49618fe1dded7606a59a17bb53d5d41d55ff791a
Brandon Presley <brandon@presley.nz>
2024-03-04 16:48:18 +1300

    Automatically create github releases on deploy

--- deploy.ps1
+++ deploy.ps1
@@ -5,6 +5,7 @@ if ($pubspecContent -match 'version: (\d+\.\d+\.\d+)\+(\d+)') {
     $buildNumber = [int]$matches[2]
     $newBuildNumber = $buildNumber + 1
     $newVersion = "version: $version+$newBuildNumber"
+    $lastCommit = git log -1 --pretty=%B
 
     $pubspecContent = $pubspecContent -replace 'version: (\d+\.\d+\.\d+)\+(\d+)', $newVersion
     Set-Content -Path "pubspec.yaml" -Value $pubspecContent
@@ -16,6 +17,9 @@ if ($pubspecContent -match 'version: (\d+\.\d+\.\d+)\+(\d+)') {
     cd android
     fastlane supply --aab ..\build\app\outputs\bundle\release\app-release.aab
     git push
+
+    flutter build apk
+    gh release create "$version" --notes "$lastCommit" ..\build\app\outputs\flutter-apk\app-release.apk
 } else {
     Write-Host "Failed to update version in pubspec.yaml."
 }
