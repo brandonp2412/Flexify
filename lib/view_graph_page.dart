3aef777e01e3ec9335e1c117edc4cc24d8917c25
Brandon Presley <brandon@presley.nz>
2024-03-19 12:23:01 +1300

    Stop pushing graph after going back

--- lib/view_graph_page.dart
+++ lib/view_graph_page.dart
@@ -1,9 +1,11 @@
 import 'package:drift/drift.dart' as drift;
 import 'package:fl_chart/fl_chart.dart';
+import 'package:flexify/app_state.dart';
 import 'package:flexify/constants.dart';
 import 'package:flexify/graph_history.dart';
 import 'package:flexify/main.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class GraphData {
   final String created;
@@ -73,6 +75,7 @@ class _ViewGraphPageState extends State<ViewGraphPage>
           icon: const Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pop(context);
+            context.read<AppState>().selectExercise("");
           },
         ),
         actions: [
