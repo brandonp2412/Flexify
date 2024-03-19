a0697e8d1f707cea1fbcfe53c86da726f859ef46
Brandon Presley <brandon@presley.nz>
2024-03-19 22:42:57 +1300

    Optimize exercise list

--- lib/exercise_list.dart
+++ lib/exercise_list.dart
@@ -23,11 +23,10 @@ class ExerciseList extends StatelessWidget {
       itemCount: planExercises.length,
       itemBuilder: (context, index) {
         final exercise = planExercises[index];
-        final gymSets = snapshot.data?.where(
+        final gymSet = snapshot.data?.firstWhere(
             (element) => element.read(database.gymSets.name) == exercise);
         var count = 0;
-        if (gymSets != null && gymSets.isNotEmpty)
-          count = gymSets.first.read(database.gymSets.name.count())!;
+        if (gymSet != null) count = gymSet.read(database.gymSets.name.count())!;
         return ExerciseTile(
           index: index,
           exercise: exercise,
