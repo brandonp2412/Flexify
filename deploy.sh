01211a68dd8c0c6895bb55ab4726643bf2e18878
Brandon Presley <brandon@presley.nz>
2024-04-04 13:47:10 +1300

    Bump version to 1.0.28

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-set -ex
+set -e
 
 pubspec_content=$(cat pubspec.yaml)
 
 function generate_screenshots() {
-    device_type=$1
-    export FLEXIFY_DEVICE_TYPE="$device_type"
-    flutter drive --driver=test_driver/integration_test.dart --target=integration_test/screenshot_test.dart --dart-define=FLEXIFY_DEVICE_TYPE="$device_type" --profile -d "$device_type"
+    devices=$(adb devices | grep emulator)
+
+    while read line
+    do
+      emulator_id=$(echo $line | awk '{print $1}')
+      name=$(adb -s $emulator_id emu avd name | head -n 1) 
+      [ "$name" = "$1" ] && break
+    done < <(echo $devices)
+
+    export FLEXIFY_DEVICE_TYPE="$1"
+    flutter drive --driver=test_driver/integration_test.dart --target=integration_test/screenshot_test.dart --dart-define=FLEXIFY_DEVICE_TYPE="$1" --profile -d "$emulator_id"
 }
 
 line=$(yq -r .version pubspec.yaml)
@@ -20,7 +28,8 @@ new_patch=$((patch + 1))
 new_build_number=$((build_number + 1))
 
 last_commit=$(git log -1 --pretty=%B | head -n 1)
-yq -yi ".version |= \"$major.$minor.$new_patch+$new_build_number\"" pubspec.yaml
+new_version="$major.$minor.$new_patch+$new_build_number"
+yq -yi ".version |= \"$new_version\"" pubspec.yaml
 generate_screenshots "phoneScreenshots"
 
 git add pubspec.yaml
@@ -36,7 +45,7 @@ cd android
 flutter build appbundle
 fastlane supply --aab ../build/app/outputs/bundle/release/app-release.aab
 flutter build apk
-gh release create "$version" --notes "$last_commit" ../build/app/outputs/flutter-apk/app-release.apk
+gh release create "$new_version" --notes "$last_commit" ../build/app/outputs/flutter-apk/app-release.apk
 git push --tags
 git push
 
