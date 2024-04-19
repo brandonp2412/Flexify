b07f582a28de6f836de5c6e3b06b87f4324c6c58
Brandon Presley <brandon@presley.nz>
2024-04-19 19:30:55 +1200

    Split up deploy.sh and screenshots.sh

--- deploy.sh
+++ deploy.sh
@@ -2,30 +2,9 @@
 
 set -ex
 
-function generate_screenshots() {
-  $TERMINAL -t Emulator emulator -avd "$1" -feature -Vulkan -no-boot-anim -noaudio &> /dev/null &
-
-  while true; do  
-    for device in $(adb devices | awk 'NR>1{print $1}' | grep emulator); do
-      name=$(adb -s $device emu avd name | head -n 1 | tr -d '\r') 
-      [ "$name" = "$1" ] && break
-    done
-    boot_completed=$(adb -s "$device" shell getprop sys.boot_completed | tr -d '\r')
-    adb -s "$device" get-state | grep -q device && [ "$name" = "$1" ] && [ "$boot_completed" = "1" ] \
-      && break
-    sleep 1
-  done
-
-  export FLEXIFY_DEVICE_TYPE="$1"
-  flutter drive --driver=test_driver/integration_test.dart \
-    --target=integration_test/screenshot_test.dart \
-    --dart-define=FLEXIFY_DEVICE_TYPE="$1" --profile -d "$device"
-  adb -s "$device" reboot -p
-}
-
-generate_screenshots "phoneScreenshots"
-generate_screenshots "sevenInchScreenshots"
-generate_screenshots "tenInchScreenshots"
+./screenshots.sh "phoneScreenshots"
+./screenshots.sh "sevenInchScreenshots"
+./screenshots.sh "tenInchScreenshots"
 
 line=$(yq -r .version pubspec.yaml)
 build_number=$(cut -d '+' -f 2 <<< "$line")
---
+++ screenshots.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+$TERMINAL -t Emulator emulator -avd "$1" -feature -Vulkan -no-boot-anim -noaudio &> /dev/null &
+
+while true; do  
+for device in $(adb devices | awk 'NR>1{print $1}' | grep emulator); do
+  name=$(adb -s $device emu avd name | head -n 1 | tr -d '\r') 
+  [ "$name" = "$1" ] && break
+done
+boot_completed=$(adb -s "$device" shell getprop sys.boot_completed | tr -d '\r')
+adb -s "$device" get-state | grep -q device && [ "$name" = "$1" ] && [ "$boot_completed" = "1" ] \
+  && break
+sleep 1
+done
+
+export FLEXIFY_DEVICE_TYPE="$1"
+flutter drive --driver=test_driver/integration_test.dart \
+--target=integration_test/screenshot_test.dart \
+--dart-define=FLEXIFY_DEVICE_TYPE="$1" --profile -d "$device"
+adb -s "$device" reboot -p
