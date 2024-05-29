0e9953f80a04ec3d4bdfbefc97d7d406f1d5caa7
Brandon Presley <brandon@presley.nz>
2024-05-29 16:38:51 +1200

    Test inserting gym sets

--- test/edit_gym_set_test.dart
+++ test/edit_gym_set_test.dart
@@ -1,5 +1,6 @@
 import 'package:flexify/database.dart';
 import 'package:flexify/edit_gym_set.dart';
+import 'package:flexify/main.dart';
 import 'package:flexify/plan_state.dart';
 import 'package:flexify/settings_state.dart';
 import 'package:flexify/timer_state.dart';
@@ -10,9 +11,50 @@ import 'package:provider/provider.dart';
 import 'mock_tests.dart';
 
 void main() async {
-  await mockTests();
+  testWidgets('EditGymSet inserts', (WidgetTester tester) async {
+    await mockTests();
+    await tester.pumpWidget(
+      MultiProvider(
+        providers: [
+          ChangeNotifierProvider(create: (context) => SettingsState()),
+          ChangeNotifierProvider(create: (context) => TimerState()),
+          ChangeNotifierProvider(create: (context) => PlanState()),
+        ],
+        child: MaterialApp(
+          home: EditGymSet(
+            gymSet: GymSet(
+              id: 0,
+              name: "Bench press",
+              reps: 2,
+              weight: 3,
+              unit: 'kg',
+              created: DateTime.now(),
+              hidden: false,
+              bodyWeight: 52,
+              duration: 3,
+              distance: 6,
+              cardio: false,
+            ),
+          ),
+        ),
+      ),
+    );
 
-  testWidgets('HomePage Widget Test', (WidgetTester tester) async {
+    expect(find.text("Bench press"), findsOne);
+
+    final button = find.byTooltip("Save");
+    expect(button, findsOne);
+
+    await tester.tap(button);
+    await tester.pumpAndSettle();
+
+    expect(find.text('Bench press'), findsNothing);
+
+    await db.close();
+  });
+
+  testWidgets('EditGymSet updates', (WidgetTester tester) async {
+    await mockTests();
     await tester.pumpWidget(
       MultiProvider(
         providers: [
@@ -47,5 +89,9 @@ void main() async {
 
     await tester.tap(button);
     await tester.pumpAndSettle();
+
+    expect(find.text('Bench press'), findsNothing);
+
+    await db.close();
   });
 }
