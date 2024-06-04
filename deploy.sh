5850d5cc94c67f0c726b6d1a1bf871f47dd52199
Brandon Presley <brandon@presley.nz>
2024-06-05 10:58:05 +1200

    Fix deploy.sh - 1.1.14 🚀

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -ex
 
 ./flutter/bin/flutter test
 
-dart run drift_dev schema dump lib/database.dart drift_schemas
+dart run drift_dev schema dump lib/database/database.dart drift_schemas
 dart run drift_dev schema steps drift_schemas/ lib/database/schema_versions.dart
 dart run drift_dev schema generate drift_schemas/ test/generated_migrations/
 if [[ -n "$(git diff --stat)" ]]; then
