046803bd61118a17ec3c8c7a6ccf3845c8a3aaf9
Brandon Presley <brandon@presley.nz>
2024-06-11 10:37:38 +1200

    Add ios screenshots

--- screenshots.sh
+++ screenshots.sh
@@ -1,25 +1,34 @@
 #!/bin/sh
 
-$TERMINAL -t Hide emulator -avd "$1" -feature -Vulkan -no-boot-anim -noaudio &> /dev/null &
+set -ex
 
-while true; do  
-for device in $(adb devices | awk 'NR>1{print $1}' | grep emulator); do
-  name=$(adb -s $device emu avd name | head -n 1 | tr -d '\r') 
-  [ "$name" = "$1" ] && break
-done
-boot_completed=$(adb -s "$device" shell getprop sys.boot_completed | tr -d '\r')
-adb -s "$device" get-state | grep -q device && [ "$name" = "$1" ] && [ "$boot_completed" = "1" ] \
-  && break
-sleep 1
-done
+case "$(uname -s)" in 
+  Linux*)
+    $TERMINAL -t Hide emulator -avd "$1" -feature -Vulkan -no-boot-anim -noaudio &> /dev/null &
+
+    while true; do  
+      for device in $(adb devices | awk 'NR>1{print $1}' | grep emulator); do
+        name=$(adb -s $device emu avd name | head -n 1 | tr -d '\r') 
+        [ "$name" = "$1" ] && break
+      done
+      boot_completed=$(adb -s "$device" shell getprop sys.boot_completed | tr -d '\r')
+      adb -s "$device" get-state | grep -q device && [ "$name" = "$1" ] && [ "$boot_completed" = "1" ] \
+        && break
+      sleep 1
+    done
+  ;;
+esac
 
 export FLEXIFY_DEVICE_TYPE="$1"
 
 flutter drive --driver=test_driver/integration_test.dart \
   --target=integration_test/screenshot_test.dart \
-  --dart-define=FLEXIFY_DEVICE_TYPE="$1" -d "$device"
+  --dart-define=FLEXIFY_DEVICE_TYPE="$1" -d "${device:-$1}"
 code=$?
 
-adb -s "$device" reboot -p
-
-exit $code
+case "$(uname -s)" in 
+  Linux*)
+    adb -s "$device" reboot -p
+    exit $code
+  ;;
+esac
