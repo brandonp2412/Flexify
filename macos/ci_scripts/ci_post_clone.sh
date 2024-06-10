00606e8ccbf8e2a005675df4db0e75306bb6f8db
Brandon Presley <brandon@presley.nz>
2024-06-10 12:09:53 +1200

    Add ci_post_clone script for xcode cloud

---
+++ macos/ci_scripts/ci_post_clone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+cd $CI_PRIMARY_REPOSITORY_PATH
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
+export PATH="$PATH:$HOME/flutter/bin"
+
+flutter precache --macos
+flutter pub get
+HOMEBREW_NO_AUTO_UPDATE=1
+brew install cocoapods
+cd macos && pod install
+
+exit 0
