a48d00393830c7422c3acada32d0ed99d5c8e519
Brandon Presley <brandon@presley.nz>
2024-06-11 13:51:07 +1200

    Make editing gym sets scrollable - 1.1.23 🚀
    On small screens (i.e. iphone SE) the
    keyboard coming up covers the screen
    and the fields need to become
    scrollable.

--- macos/ci_scripts/ci_post_clone.sh
+++ macos/ci_scripts/ci_post_clone.sh
@@ -3,8 +3,7 @@
 set -e
 
 cd $CI_PRIMARY_REPOSITORY_PATH
-git clone https://github.com/flutter/flutter.git --depth 1 $HOME/flutter
-export PATH="$PATH:$HOME/flutter/bin"
+export PATH="$PATH:$(pwd)/flutter/bin"
 
 flutter precache --macos
 flutter pub get
