2dbdda3cd1bdc904c3a4400a61b7a6bdd88ec7dd
Joseph <josephnglynn@hotmail.co.uk>
2024-06-22 22:49:54 +0100

    Added clock.h for clock typedef

---
+++ native/flexify_native/clock.h
@@ -0,0 +1,13 @@
+//
+// Created by joseph on 22/06/24.
+//
+
+#ifndef NATIVE_CLOCK_H
+#define NATIVE_CLOCK_H
+#include <chrono>
+
+namespace flexify {
+    using fclock_t = std::chrono::system_clock;
+}
+
+#endif //NATIVE_CLOCK_H
