298a2f18da637f1dad0e967155c8b9db8fd078a8
Brandon Presley <brandon@presley.nz>
2024-07-07 15:26:22 +1200

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -ex
 
@@ -6,7 +6,7 @@ dart run drift_dev schema dump lib/database/database.dart drift_schemas
 dart run drift_dev schema generate drift_schemas/ test/generated_migrations/
 dart run drift_dev schema steps drift_schemas/ lib/database/schema_versions.dart
 
-if [[ -n "$(git diff --stat drift_schemas lib/database/schema_versions.dart test/generated_migrations)" ]]; then
+if [ -n "$(git diff --stat '**schema**' '**migration**')" ]; then
   echo "There are unstaged changes in the repository:"
   git --no-pager diff
   exit 1
