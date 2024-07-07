28658dc5a74959e34fb2143ae69acc17fd4808dd
Brandon Presley <brandon@presley.nz>
2024-07-07 16:05:37 +1200

    Add watch flags to migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -2,9 +2,22 @@
 
 set -ex
 
-dart run drift_dev schema dump lib/database/database.dart drift_schemas
-dart run drift_dev schema generate drift_schemas/ test/generated_migrations/
-dart run drift_dev schema steps drift_schemas/ lib/database/schema_versions.dart
+migrate() {
+  dart run drift_dev schema dump lib/database/database.dart drift_schemas
+  dart run drift_dev schema generate drift_schemas/ test/generated_migrations/
+  dart run drift_dev schema steps drift_schemas/ lib/database/schema_versions.dart
+}
+
+if [ "$1" = "--watch" ] || [ "$1" = "-w" ]; then
+  find lib -type f | entr -s "
+    dart run drift_dev schema dump lib/database/database.dart drift_schemas
+    dart run drift_dev schema generate drift_schemas/ test/generated_migrations/
+    dart run drift_dev schema steps drift_schemas/ lib/database/schema_versions.dart
+  "
+  exit 0
+fi
+
+migrate
 
 if [ -n "$(git diff --stat '**schema**' '**migration**')" ]; then
   echo "There are unstaged changes in the repository:"
