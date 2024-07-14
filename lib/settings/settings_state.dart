c2a3f61432b6f4aa6a56273e2ef405f4537058a6
Brandon Presley <brandon@presley.nz>
2024-07-14 13:41:26 +1200

    Clean up settings state subscription on dispose

--- lib/settings/settings_state.dart
+++ lib/settings/settings_state.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:drift/drift.dart';
 import 'package:flexify/database/database.dart';
 import 'package:flexify/main.dart';
@@ -5,14 +7,22 @@ import 'package:flutter/material.dart';
 
 class SettingsState extends ChangeNotifier {
   late Setting value;
+  StreamSubscription? subscription;
 
   SettingsState(Setting settings) {
     value = settings;
     init();
   }
 
+  @override
+  dispose() {
+    super.dispose();
+    subscription?.cancel();
+  }
+
   Future<void> init() async {
-    (db.settings.select()..limit(1)).watchSingle().listen((event) {
+    subscription =
+        (db.settings.select()..limit(1)).watchSingle().listen((event) {
       value = event;
       notifyListeners();
     });
