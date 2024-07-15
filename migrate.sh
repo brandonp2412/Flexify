f5fbe71549c6705821ea0fad000a679488daafad
Brandon Presley <brandon@presley.nz>
2024-07-15 12:20:23 +1200

    Ensure database is built before migrating

--- migrate.sh
+++ migrate.sh
@@ -3,6 +3,7 @@
 set -ex
 
 migrate() {
+  dart run build_runner build -d
   dart run drift_dev schema dump lib/database/database.dart drift_schemas
   dart run drift_dev schema generate drift_schemas/ test/generated_migrations/
   dart run drift_dev schema steps drift_schemas/ lib/database/schema_versions.dart
