20e7970373c42789b7fedb8008a54a052b96f665
Brandon Presley <brandon@presley.nz>
2024-07-16 18:10:43 +1200

    Update screenshots.sh

--- screenshots.sh
+++ screenshots.sh
@@ -1,29 +1,41 @@
-#!/bin/sh
+#!/bin/bash
 
-set -ex
+set -e
+
+device_type="${1:-phoneScreenshots}"
 
 case "$(uname -s)" in 
   Linux*)
-    $TERMINAL -t Hide emulator -avd "$1" -feature -Vulkan -no-boot-anim -noaudio &> /dev/null &
+    echo "Launching $device_type..."
+    $TERMINAL -t Hide emulator -avd "$device_type" -feature -Vulkan \
+      -no-boot-anim -noaudio &> /dev/null &
 
     while true; do  
       for device in $(adb devices | awk 'NR>1{print $1}' | grep emulator); do
-        name=$(adb -s $device emu avd name | head -n 1 | tr -d '\r') 
-        [ "$name" = "$1" ] && break
+        name=$(
+          adb -s "$device" emu avd name 2> /dev/null | head -n 1 | tr -d '\r'
+        ) 
+        [ "$name" = "$device_type" ] && break
       done
-      boot_completed=$(adb -s "$device" shell getprop sys.boot_completed | tr -d '\r')
-      adb -s "$device" get-state | grep -q device && [ "$name" = "$1" ] && [ "$boot_completed" = "1" ] \
+
+      boot_completed=$(
+        adb -s "$device" shell getprop sys.boot_completed 2> /dev/null \
+          | tr -d '\r'
+      )
+      adb -s "$device" get-state 2> /dev/null | grep -q device && \
+        [ "$name" = "$device_type" ] && [ "$boot_completed" = "1" ] \
         && break
+
       sleep 1
     done
   ;;
 esac
 
-export FLEXIFY_DEVICE_TYPE="$1"
+export FLEXIFY_DEVICE_TYPE="$device_type"
 
 flutter drive --driver=test_driver/integration_test.dart \
   --target=integration_test/screenshot_test.dart \
-  --dart-define=FLEXIFY_DEVICE_TYPE="$1" -d "${device:-$1}"
+  --dart-define=FLEXIFY_DEVICE_TYPE="$device_type" -d "${device:-$device_type}"
 code=$?
 
 case "$(uname -s)" in 
