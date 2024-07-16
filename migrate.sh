6f60c6fa3be5456e268a7519a25c1269f73d9842
Brandon Presley <brandon@presley.nz>
2024-07-16 18:14:40 +1200

    Remove watch flag from migrate.sh
    We shouldn't finna be watching the
    migrations in the first place.

--- migrate.sh
+++ migrate.sh
@@ -2,23 +2,10 @@
 
 set -ex
 
-migrate() {
-  dart run build_runner build -d
-  dart run drift_dev schema dump lib/database/database.dart drift_schemas
-  dart run drift_dev schema generate drift_schemas/ test/generated_migrations/
-  dart run drift_dev schema steps drift_schemas/ lib/database/schema_versions.dart
-}
-
-if [ "$1" = "--watch" ] || [ "$1" = "-w" ]; then
-  find lib -type f | entr -s "
-    dart run drift_dev schema dump lib/database/database.dart drift_schemas
-    dart run drift_dev schema generate drift_schemas/ test/generated_migrations/
-    dart run drift_dev schema steps drift_schemas/ lib/database/schema_versions.dart
-  "
-  exit 0
-fi
-
-migrate
+dart run build_runner build -d
+dart run drift_dev schema dump lib/database/database.dart drift_schemas
+dart run drift_dev schema generate drift_schemas/ test/generated_migrations/
+dart run drift_dev schema steps drift_schemas/ lib/database/schema_versions.dart
 
 if [ -n "$(git diff --stat '**schema**' '**migration**')" ]; then
   echo "There are unstaged changes in the repository:"
