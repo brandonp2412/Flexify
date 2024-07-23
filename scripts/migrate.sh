32948b041c4a441227538cd3ebee2e9d8d85511a
Brandon Presley <brandon@presley.nz>
2024-07-23 20:16:12 +1200

    Remove logic for bumping schema version

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -4,20 +4,6 @@ set -ex
 
 database=lib/database/database.dart
 
-if git ls-files --error-unmatch $database >/dev/null 2>&1; then
-  staged=$(git diff --staged HEAD -- $database)
-  unstaged=$(git diff HEAD -- $database)
-  changes="$staged$unstaged"
-
-  if echo "$changes" | grep -q "schemaVersion"; then
-    echo "Schema version already bumped! nice."
-  else
-    old=$(grep -oP 'schemaVersion => \K\d+' $database)
-    new=$((old + 1))
-    sed -i "s/schemaVersion => $old/schemaVersion => $new/" $database
-  fi
-fi
-
 dart run build_runner build -d
 dart run drift_dev schema dump $database drift_schemas
 dart run drift_dev schema generate drift_schemas/ test/generated_migrations/
