a548b418d06cf4071dcc579498975dee132a95f1
Brandon Presley <brandon@presley.nz>
2024-07-25 13:47:36 +1200

    Only log errors in scripts/migrate.sh

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -4,10 +4,10 @@ set -ex
 
 database=lib/database/database.dart
 
-dart run build_runner build -d
-dart run drift_dev schema dump $database drift_schemas
-dart run drift_dev schema generate drift_schemas/ test/generated_migrations/
-dart run drift_dev schema steps drift_schemas/ lib/database/schema_versions.dart
+dart run --verbosity=error build_runner build -d
+dart run --verbosity=error drift_dev schema dump $database drift_schemas
+dart run --verbosity=error drift_dev schema generate drift_schemas/ test/generated_migrations/
+dart run --verbosity=error drift_dev schema steps drift_schemas/ lib/database/schema_versions.dart
 
 if [ -n "$(git diff --stat '**schema**' '**migration**')" ]; then
   echo "There are unstaged changes in the repository:"
