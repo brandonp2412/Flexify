aafcc3c88c026efc95718cfb081d7672e3dba060
Brandon Presley <brandon@presley.nz>
2024-12-25 17:55:43 +1300

    Use nested route on timer -> settings
    Fixes settings page covering the tabs
    from the timer page.

--- lib/timer/timer_page.dart
+++ lib/timer/timer_page.dart
@@ -1,4 +1,5 @@
 import 'package:flexify/settings/settings_page.dart';
+import 'package:flexify/settings/settings_state.dart';
 import 'package:flexify/timer/timer_progress_widgets.dart';
 import 'package:flexify/timer/timer_state.dart';
 import 'package:flutter/material.dart';
@@ -12,9 +13,41 @@ class TimerPage extends StatefulWidget {
 }
 
 class TimerPageState extends State<TimerPage> {
+  final GlobalKey<NavigatorState> navigatorKey = GlobalKey<NavigatorState>();
+
   @override
   Widget build(BuildContext context) {
     final timerState = context.watch<TimerState>();
+
+    return NavigatorPopHandler(
+      onPop: () {
+        if (navigatorKey.currentState!.canPop() == false) return;
+        final tabController = DefaultTabController.of(context);
+        final settings = context.read<SettingsState>().value;
+        final historyIndex = settings.tabs.split(',').indexOf('HistoryPage');
+        if (tabController.index == historyIndex)
+          navigatorKey.currentState!.pop();
+      },
+      child: Navigator(
+        key: navigatorKey,
+        onGenerateRoute: (settings) => MaterialPageRoute(
+          builder: (context) => _TimerPageWidget(timerState: timerState),
+          settings: settings,
+        ),
+      ),
+    );
+  }
+}
+
+class _TimerPageWidget extends StatelessWidget {
+  const _TimerPageWidget({
+    required this.timerState,
+  });
+
+  final TimerState timerState;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         actions: [
