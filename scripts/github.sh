b7dadd8dd798374928cb744209da97efc01d6250
Brandon Presley <brandon@presley.nz>
2025-02-01 11:12:16 +1300

    Remove project name factoring in github.sh
    It's so much nicer when we can just copy
    paste certain lines into the terminal.

--- scripts/github.sh
+++ scripts/github.sh
@@ -5,22 +5,21 @@ apk=$PWD/build/app/outputs/flutter-apk
 ./flutter/bin/flutter build apk --split-per-abi
 adb -d install "$apk"/app-arm64-v8a-release.apk || true
 ./flutter/bin/flutter build apk
-project=$(basename "$PWD")
-mv -f "$apk"/app-release.apk "$apk/$project.apk"
+mv -f "$apk"/app-release.apk "$apk/flexify.apk"
 ./flutter/bin/flutter build appbundle
 
 mkdir -p build/native_assets/linux
 ./flutter/bin/flutter build linux
-(cd "$apk/pipeline/linux/x64/release/bundle" && zip --quiet -r "$project-linux.zip" .)
+(cd "$apk/pipeline/linux/x64/release/bundle" && zip --quiet -r "flexify-linux.zip" .)
 
 docker start windows
 rsync -a --delete --exclude-from=.gitignore ./* .gitignore \
-  "$HOME/windows/$project-source"
+  "$HOME/windows/flexify-source"
 while ! ssh windows exit; do sleep 1; done
 ssh windows "Powershell -ExecutionPolicy bypass -File //host.lan/Data/build-flexify.ps1"
-sudo chown -R "$USER" "$HOME/windows/$project"
-mv -f "$HOME/windows/$project/$project.msix" "$HOME/windows/$project.msix"
-(cd "$HOME/windows/$project" && zip --quiet -r "$HOME/windows/$project-windows.zip" .)
+sudo chown -R "$USER" "$HOME/windows/flexify"
+mv -f "$HOME/windows/flexify/flexify.msix" "$HOME/windows/flexify.msix"
+(cd "$HOME/windows/flexify" && zip --quiet -r "$HOME/windows/flexify-windows.zip" .)
 docker stop windows
 
 git add pubspec.lock
@@ -32,7 +31,7 @@ changelog=$(cat fastlane/metadata/android/en-US/changelogs/$changelog_number.txt
 
 gh release create "$major.$minor.$patch" --notes "$changelog" \
   "$apk"/app-*-release.apk \
-  "$apk/pipeline/linux/x64/release/bundle/$project-linux.zip" \
-  "$apk/$project.apk" \
-  "$HOME/windows/$project-windows.zip"
+  "$apk/pipeline/linux/x64/release/bundle/flexify-linux.zip" \
+  "$apk/flexify.apk" \
+  "$HOME/windows/flexify-windows.zip"
 git pull origin HEAD
