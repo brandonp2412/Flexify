94d9ed593d7057adbfc3f66e371d1dd5bd9b5c8c
Brandon Presley <brandon@presley.nz>
2025-02-04 15:51:13 +1300

    Use latest make-migrations script for drift

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,12 +2,8 @@
 
 set -ex
 
-database=lib/database/database.dart
-
 dart run --verbosity=error build_runner build -d
-dart run --verbosity=error drift_dev schema dump $database drift_schemas
-dart run --verbosity=error drift_dev schema generate drift_schemas/ test/generated_migrations/
-dart run --verbosity=error drift_dev schema steps drift_schemas/ lib/database/schema_versions.dart
+dart run --verbosity=error drift_dev make-migrations
 
 if [ -n "$(git diff --stat '**schema**' '**migration**')" ]; then
   echo "There are unstaged changes in the repository:"
