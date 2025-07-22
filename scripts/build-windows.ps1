5c7cfbe02c56524620982dd0ae38e36eb6b1231c
Brandon Presley <brandon@presley.nz>
2025-07-22 21:46:02 +1200

    Delete unused scripts

--- scripts/build-windows.ps1
+++
@@ -1,5 +0,0 @@
-Remove-Item -Path flexify/* -Recurse -Force
-robocopy //host.lan/Data/flexify-source flexify /E /Z /R:3 /NFL /NDL /NP /XD build*
-cd flexify
-dart run msix:create
-robocopy build/windows/x64/runner/Release //host.lan/Data/flexify /E /Z /R:3 /NFL /NDL /NP /XD build*
