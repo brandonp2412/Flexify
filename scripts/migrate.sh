36b9703f94148bf9fca3d3ff7e176415393c11c2
Brandon Presley <brandon@presley.nz>
2025-07-23 11:24:15 +1200

    Remove diffing from migrate.sh

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -5,9 +5,3 @@ set -ex
 ./flutter/bin/dart run build_runner build -d
 ./flutter/bin/dart run drift_dev make-migrations
 ./flutter/bin/dart run drift_dev schema generate drift_schemas/ test/generated_migrations/
-
-if [ -n "$(git diff --stat '**schema**' '**migration**')" ]; then
-  echo "There are unstaged changes in the repository:"
-  git --no-pager diff
-  exit 1
-fi
