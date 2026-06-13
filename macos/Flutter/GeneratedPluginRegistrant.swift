3381cb4c6bd9a9b3dd8f1820b67a9fe80001a75d
Brandon Presley <brandon@presley.nz>
2026-06-13 13:04:09 +1200

    chore: fix android build warnings, upgrade gradle to 8.14
    - Upgrade Gradle wrapper from 8.13 to 8.14
    - Set builtInKotlin=true, newDsl=true, Java 17 home with workers.max=6
    - Fix Java source/target 8 warnings via -Xlint:-options
    - Add flutter-gradle-plugin to pluginManagement
    - Update generated platform plugin registrants from pub upgrade

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,6 @@ import dynamic_color
 import file_picker
 import flutter_local_notifications
 import package_info_plus
-import path_provider_foundation
 import share_plus
 import sqlite3_flutter_libs
 import url_launcher_macos
@@ -22,7 +21,6 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   Sqlite3FlutterLibsPlugin.register(with: registry.registrar(forPlugin: "Sqlite3FlutterLibsPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
