71b8a4699a75c761b17278f7688dadc3c47b6db6
Brandon Dick <brandon@presley.nz>
2026-08-27 17:29:31 +1200

    test: decouple cardio widgets from screenshot tests

--- test/cardio_page_test.dart
+++ test/cardio_page_test.dart
@@ -3,65 +3,55 @@ import 'package:fl_chart/fl_chart.dart';
 import 'package:flexify/constants.dart';
 import 'package:flexify/database/gym_sets.dart';
 import 'package:flexify/graph/cardio_page.dart';
-import 'package:flexify/main.dart';
-import 'package:flexify/plan/plan_state.dart';
-import 'package:flexify/settings/settings_state.dart';
-import 'package:flexify/timer/timer_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:provider/provider.dart';
 
-import '../integration_test/screenshot_test.dart';
 import 'mock_tab_controller.dart';
-import 'mock_tests.dart';
+import 'support/graph_fixtures.dart';
+import 'support/test_app.dart';
 
-void main() async {
-  testWidgets('CardioPage displays', (WidgetTester tester) async {
-    await mockTests();
-    db = testDb();
-    for (final element in graphData) {
-      await db
-          .into(db.gymSets)
-          .insert(
-            generateGymSetCompanion(
-              "Run",
-              element.weight,
-              reps: element.reps,
-              date: element.dateTime,
-            ).copyWith(cardio: const Value(true)),
-          );
-    }
+Future<void> pumpCardioPage(
+  WidgetTester tester,
+  FlexifyTestHarness harness,
+) async {
+  for (final element in graphData) {
+    await harness.database.into(harness.database.gymSets).insert(
+      graphGymSet(
+        'Run',
+        element.weight,
+        reps: element.reps,
+        date: element.dateTime,
+      ).copyWith(cardio: const Value(true)),
+    );
+  }
 
-    final settings = await (db.settings.select()..limit(1)).getSingle();
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (context) => SettingsState(settings)),
-          ChangeNotifierProvider(create: (context) => TimerState()),
-          ChangeNotifierProvider(create: (context) => PlanState()),
-        ],
-        child: MaterialApp(
-          home: DefaultTabController(
-            length: 1,
-            child: CardioPage(
-              tabCtrl: MockTabController(),
-              name: 'Run',
-              unit: 'km',
-              data: await getCardioData(
-                target: 'km',
-                name: 'Run',
-                metric: CardioMetric.pace,
-                period: Period.day,
-                start: null,
-                end: null,
-              ),
-            ),
-          ),
+  await harness.pump(
+    tester,
+    DefaultTabController(
+      length: 1,
+      child: CardioPage(
+        tabCtrl: MockTabController(),
+        name: 'Run',
+        unit: 'km',
+        data: await getCardioData(
+          target: 'km',
+          name: 'Run',
+          metric: CardioMetric.pace,
+          period: Period.day,
+          start: null,
+          end: null,
         ),
       ),
-    );
+    ),
+  );
+  await tester.pumpAndSettle();
+}
 
-    await tester.pumpAndSettle();
+void main() {
+  testWidgets('CardioPage displays', (WidgetTester tester) async {
+    final harness = await FlexifyTestHarness.create();
+    await pumpCardioPage(tester, harness);
+
     expect(find.text('Run'), findsOne);
     expect(find.text('Pace (distance / time)'), findsOne);
     expect(find.byTooltip('Edit'), findsOne);
@@ -74,114 +64,26 @@ void main() async {
   });
 
   testWidgets('CardioPage edits', (WidgetTester tester) async {
-    await mockTests();
-    db = testDb();
-    for (final element in graphData) {
-      await db
-          .into(db.gymSets)
-          .insert(
-            generateGymSetCompanion(
-              "Run",
-              element.weight,
-              reps: element.reps,
-              date: element.dateTime,
-            ).copyWith(cardio: const Value(true)),
-          );
-    }
+    final harness = await FlexifyTestHarness.create();
+    await pumpCardioPage(tester, harness);
 
-    final settings = await (db.settings.select()..limit(1)).getSingle();
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (context) => SettingsState(settings)),
-          ChangeNotifierProvider(create: (context) => TimerState()),
-          ChangeNotifierProvider(create: (context) => PlanState()),
-        ],
-        child: MaterialApp(
-          home: DefaultTabController(
-            length: 1,
-            child: CardioPage(
-              tabCtrl: MockTabController(),
-              name: 'Run',
-              unit: 'km',
-              data: await getCardioData(
-                target: 'km',
-                name: 'Run',
-                metric: CardioMetric.pace,
-                period: Period.day,
-                start: null,
-                end: null,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-
-    await tester.pumpAndSettle();
-    final edit = find.byTooltip('Edit');
-    await tester.tap(edit);
+    await tester.tap(find.byTooltip('Edit'));
     await tester.pumpAndSettle();
     expect(find.text('Update all run'), findsOne);
   });
 
   testWidgets('CardioPage selects metrics', (WidgetTester tester) async {
-    await mockTests();
-    db = testDb();
-    for (final element in graphData) {
-      await db
-          .into(db.gymSets)
-          .insert(
-            generateGymSetCompanion(
-              "Run",
-              element.weight,
-              reps: element.reps,
-              date: element.dateTime,
-            ).copyWith(cardio: const Value(true)),
-          );
+    final harness = await FlexifyTestHarness.create();
+    await pumpCardioPage(tester, harness);
+
+    var currentMetric = 'Pace (distance / time)';
+    for (final metric in ['Duration', 'Distance']) {
+      await tester.tap(find.text(currentMetric));
+      await tester.pumpAndSettle();
+      await tester.tap(find.text(metric));
+      await tester.pumpAndSettle();
+      expect(find.byType(LineChart), findsOne);
+      currentMetric = metric;
     }
-
-    final settings = await (db.settings.select()..limit(1)).getSingle();
-    await tester.pumpWidget(
-      MultiProvider(
-        providers: [
-          ChangeNotifierProvider(create: (context) => SettingsState(settings)),
-          ChangeNotifierProvider(create: (context) => TimerState()),
-          ChangeNotifierProvider(create: (context) => PlanState()),
-        ],
-        child: MaterialApp(
-          home: DefaultTabController(
-            length: 1,
-            child: CardioPage(
-              tabCtrl: MockTabController(),
-              name: 'Run',
-              unit: 'km',
-              data: await getCardioData(
-                target: 'km',
-                name: 'Run',
-                metric: CardioMetric.pace,
-                period: Period.day,
-                start: null,
-                end: null,
-              ),
-            ),
-          ),
-        ),
-      ),
-    );
-
-    await tester.pumpAndSettle();
-
-    await tester.tap(find.text('Pace (distance / time)'));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('Duration'));
-    await tester.pumpAndSettle();
-    expect(find.byType(LineChart), findsOne);
-
-    await tester.tap(find.text('Duration'));
-    await tester.pumpAndSettle();
-    await tester.tap(find.text('Distance'));
-    await tester.pumpAndSettle();
-    expect(find.byType(LineChart), findsOne);
   });
 }
