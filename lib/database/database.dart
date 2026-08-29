brandonp2412/FitBook

feat: integrate Talker logging

lib/database/database.dart
@@ -13,6 +13,7 @@ import 'package:fit_book/database/meals.dart';
 import 'package:fit_book/database/metadata.dart';
 import 'package:fit_book/database/settings.dart';
 import 'package:fit_book/database/weights.dart';
+import 'package:fit_book/logging.dart';
 import 'package:fit_book/main.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart' as material;
@@ -37,6 +38,7 @@ class AppDatabase extends _$AppDatabase {
   MigrationStrategy get migration {
     return MigrationStrategy(
       onCreate: (Migrator m) async {
+        talker.info('Creating FitBook database');
         await m.createAll();
 
         final blob = await rootBundle.load('assets/food-data.zip');
@@ -59,8 +61,12 @@ class AppDatabase extends _$AppDatabase {
         await db.foods.insertAll(insertables);
 
         await settings.insertOne(defaultSettings);
+        talker.info(
+          'Created FitBook database with ${insertables.length} starter foods',
+        );
       },
       onUpgrade: (m, from, to) async {
+        talker.info('Migrating FitBook database from v$from to v$to');
         // Following the advice from https://drift.simonbinder.eu/Migrations/api/#general-tips
         await customStatement('PRAGMA foreign_keys = OFF');
 
@@ -83,8 +89,10 @@ class AppDatabase extends _$AppDatabase {
         }
 
         await customStatement('PRAGMA foreign_keys = ON');
+        talker.info('Migrated FitBook database to v$to');
       },
       beforeOpen: (details) async {
+        talker.debug('Opening FitBook database schema v${details.versionNow}');
         // A device that already attempted the 51 -> 52 migration may have
         // reached schema 52 before its old orphaned rows were reported. Make
         // the repair idempotent so that database can recover on the next app

lib/settings/export_data.dart
@@ -3,6 +3,7 @@ import 'package:drift/drift.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:fit_book/constants.dart';
 import 'package:fit_book/main.dart';
+import 'package:fit_book/logging.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
@@ -70,6 +71,9 @@ class _ExportDataState extends State<ExportData> {
                         await FilePicker.saveFile(
                           fileName: 'diary.csv',
                           bytes: bytes,
+                        );
+                        talker.info(
+                          'Exported ${diaries.length} diary entries to CSV',
                         );
                       } finally {
                         if (mounted)
@@ -228,6 +232,7 @@ class _ExportDataState extends State<ExportData> {
                           fileName: 'foods.csv',
                           bytes: bytes,
                         );
+                        talker.info('Exported ${foods.length} foods to CSV');
                       } finally {
                         if (mounted)
                           setState(() {
@@ -258,6 +263,7 @@ class _ExportDataState extends State<ExportData> {
                           fileName: 'fitbook-backup.zip',
                           bytes: await archive.readAsBytes(),
                         );
+                        talker.info('Exported FitBook database backup');
                       } finally {
                         await workingDirectory.delete(recursive: true);
                         if (mounted) setState(() => exporting = false);

lib/settings/import_data.dart
@@ -7,6 +7,7 @@ import 'package:file_picker/file_picker.dart';
 import 'package:fit_book/database/database.dart';
 import 'package:fit_book/diary/diary_state.dart';
 import 'package:fit_book/main.dart';
+import 'package:fit_book/logging.dart';
 import 'package:fit_book/settings/settings_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -207,13 +208,15 @@ class _ImportDataState extends State<ImportData> {
 
       await db.foods.deleteAll();
       await db.foods.insertAll(foods);
+      talker.info('Imported ${foods.length} foods from CSV');
       if (widget.pageContext.mounted)
         Navigator.pushNamedAndRemoveUntil(
           widget.pageContext,
           '/',
           (_) => false,
         );
-    } catch (error) {
+    } catch (error, stackTrace) {
+      talker.handle(error, stackTrace, 'Failed to import foods CSV');
       if (!context.mounted) return;
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
@@ -263,6 +266,7 @@ class _ImportDataState extends State<ImportData> {
         db = AppDatabase();
       }
       if (!widget.pageContext.mounted) return;
+      talker.info('Imported FitBook database backup');
       Navigator.pop(widget.pageContext);
       entriesState.limit = 100;
       settingsState.setSubscription();
@@ -303,6 +307,7 @@ class _ImportDataState extends State<ImportData> {
 
     await db.diaries.deleteAll();
     await db.diaries.insertAll(diaries);
+    talker.info('Imported ${diaries.length} diary entries from CSV');
     if (widget.pageContext.mounted)
       Navigator.pushNamedAndRemoveUntil(
         widget.pageContext,

lib/settings/whats_new.dart
@@ -1,3 +1,4 @@
+import 'package:fit_book/logging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
@@ -73,7 +74,7 @@ class _WhatsNewState extends State<WhatsNew> {
         final filename = path.split('/').last.replaceAll('.txt', '');
         final timestamp = int.tryParse(filename);
         if (timestamp == null || filename.isEmpty) {
-          print('Skipping invalid changelog file: $path');
+          talker.warning('Skipping invalid changelog asset: $path');
           continue;
         }
         result.add(
@@ -85,8 +86,12 @@ class _WhatsNewState extends State<WhatsNew> {
             content: content,
           ),
         );
-      } catch (e) {
-        print('Error loading changelog file $path: $e');
+      } catch (error, stackTrace) {
+        talker.handle(
+          error,
+          stackTrace,
+          'Unable to load changelog asset: $path',
+        );
       }
     }
     return result;

brandonp2412/Flexify

lib/database/database.dart
@@ -9,6 +9,7 @@ import 'package:flexify/database/metadata.dart';
 import 'package:flexify/database/plan_exercises.dart';
 import 'package:flexify/database/plans.dart';
 import 'package:flexify/database/settings.dart';
+import 'package:flexify/logging.dart';
 import 'package:flutter/foundation.dart';
 
 import 'database_connection_web.dart'
@@ -37,9 +38,11 @@ class AppDatabase extends _$AppDatabase {
   MigrationStrategy get migration {
     return MigrationStrategy(
       beforeOpen: (details) async {
+        talker.debug('Opening Flexify database schema v${details.versionNow}');
         if (kDebugMode) await validateDatabaseSchema();
       },
       onCreate: (Migrator m) async {
+        talker.info('Creating Flexify database');
         await m.createAll();
         await m.createIndex(
           Index(
@@ -73,6 +76,9 @@ class AppDatabase extends _$AppDatabase {
         });
 
         await settings.insertOne(defaultSettings);
+        talker.info(
+          'Created Flexify database with starter plans and exercises',
+        );
       },
       onUpgrade: stepByStep(
         from1To2: (m, schema) async {

lib/export_data.dart
@@ -5,6 +5,7 @@ import 'package:csv/csv.dart';
 import 'package:drift/drift.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flexify/main.dart';
+import 'package:flexify/logging.dart';
 import 'package:flexify/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:path/path.dart' as p;
@@ -69,6 +70,9 @@ class ExportData extends StatelessWidget {
                         fileName: 'graphs.csv',
                         bytes: bytes,
                       );
+                      talker.info(
+                        'Exported ${gymSets.length} graph entries to CSV',
+                      );
                     },
                   ),
                   ListTile(
@@ -105,6 +109,7 @@ class ExportData extends StatelessWidget {
                         type: FileType.custom,
                         allowedExtensions: ['csv'],
                       );
+                      talker.info('Exported ${plans.length} plans to CSV');
                     },
                   ),
                   ListTile(
@@ -127,6 +132,7 @@ class ExportData extends StatelessWidget {
                           Platform.isWindows ||
                           Platform.isLinux)
                         await file.copy(result!);
+                      talker.info('Exported Flexify database backup');
                     },
                   ),
                 ],

lib/import_data.dart
@@ -6,6 +6,7 @@ import 'package:drift/drift.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flexify/database/database.dart';
 import 'package:flexify/main.dart';
+import 'package:flexify/logging.dart';
 import 'package:flexify/plan/plan_state.dart';
 import 'package:flexify/settings/settings_state.dart';
 import 'package:flexify/utils.dart';
@@ -61,6 +62,7 @@ class ImportData extends StatelessWidget {
 
   Future<void> importDatabase(BuildContext context) async {
     Navigator.pop(context);
+    talker.info('Starting Flexify database import');
 
     try {
       if (kIsWeb) {
@@ -69,6 +71,7 @@ class ImportData extends StatelessWidget {
         await _importDatabaseNative(context);
       }
     } catch (e, stackTrace) {
+      talker.handle(e, stackTrace, 'Failed to import Flexify database');
       if (!ctx.mounted) return;
       final packageInfo = await PackageInfo.fromPlatform();
       final version = packageInfo.version;
@@ -134,6 +137,7 @@ $version
     await sourceFile.copy(p.join(dbFolder.path, 'flexify.sqlite'));
     db = AppDatabase();
     dbVersion.value++;
+    talker.info('Imported Flexify database backup');
 
     await (db.settings.update()).write(
       const SettingsCompanion(alarmSound: Value('')),
@@ -266,6 +270,7 @@ $version
 
       await db.gymSets.deleteAll();
       await db.gymSets.insertAll(gymSets);
+      talker.info('Imported ${gymSets.length} graph entries');
 
       final weightSet = await getBodyWeight();
       if (weightSet != null) {
@@ -278,7 +283,8 @@ $version
       Navigator.pop(ctx);
 
       toast('Graph data imported successfully!');
-    } catch (e) {
+    } catch (e, stackTrace) {
+      talker.handle(e, stackTrace, 'Failed to import graph data');
       if (!ctx.mounted) return;
 
       toast(
@@ -362,6 +368,9 @@ $version
       await db.planExercises.deleteAll();
       await db.plans.insertAll(plansToInsert);
       await db.planExercises.insertAll(planExercisesToInsert);
+      talker.info(
+        'Imported ${plansToInsert.length} plans and ${planExercisesToInsert.length} exercises',
+      );
 
       if (!ctx.mounted) return;
       ctx.read<PlanState>().updatePlans(null);
@@ -369,6 +378,7 @@ $version
 
       toast('Plans imported successfully');
     } catch (e, stackTrace) {
+      talker.handle(e, stackTrace, 'Failed to import plan data');
       if (!ctx.mounted) return;
       final packageInfo = await PackageInfo.fromPlatform();
       final version = packageInfo.version;

lib/settings/whats_new.dart
@@ -1,3 +1,4 @@
+import 'package:flexify/logging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:intl/intl.dart';
@@ -59,7 +60,7 @@ class _WhatsNewState extends State<WhatsNew> {
         final filename = path.split('/').last.replaceAll('.txt', '');
         final timestamp = int.tryParse(filename);
         if (timestamp == null || filename.isEmpty) {
-          print('Skipping invalid changelog file: $path');
+          talker.warning('Skipping invalid changelog asset: $path');
           continue;
         }
         result.add(
@@ -71,8 +72,12 @@ class _WhatsNewState extends State<WhatsNew> {
             content: content,
           ),
         );
-      } catch (e) {
-        print('Error loading changelog file $path: $e');
+      } catch (error, stackTrace) {
+        talker.handle(
+          error,
+          stackTrace,
+          'Unable to load changelog asset: $path',
+        );
       }
     }
     return result;