b85a32aed97e6ede8e876b9f0a28c822a96dd764
Brandon Dick <brandon@presley.nz>
2026-09-10 18:11:10 +1200

    Fix permissions dialog analyzer error

--- lib/app_permissions_dialog.dart
+++ lib/app_permissions_dialog.dart
@@ -1,4 +1,4 @@
-import 'package:drift/drift.dart';
+import 'package:drift/drift.dart' hide Column;
 import 'package:flexify/database/database.dart';
 import 'package:flexify/main.dart';
 import 'package:flexify/settings/settings_state.dart';
@@ -17,7 +17,10 @@ Future<void> showAppPermissionsDialog(
   await showDialog<void>(
     context: context,
     barrierDismissible: !required,
-    builder: (context) => _AppPermissionsDialog(settings: settings),
+    builder: (context) => PopScope(
+      canPop: !required,
+      child: _AppPermissionsDialog(settings: settings),
+    ),
   );
 }
 
